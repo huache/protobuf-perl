75585a487d780e9978b29fdbe77c8104c59eee88
brad@danga.com <brad@danga.com@95d14f3d-5152-0410-9fc5-bd637da83747>
2008-07-27 01:18:05 +0000

    getting closer

--- trunk/perl/lib/Protobuf.pm
+++ trunk/perl/lib/Protobuf.pm
@@ -11,4 +11,37 @@ use Moose;
 has 'name' => (is => 'rw', isa => 'Str');
 has 'fullname' => (is => 'rw', isa => 'Str');
 
+package Protobuf::EnumValueDescriptor;
+
+use Moose::Policy 'Protobuf::AccessorNamingPolicy';
+use Moose;
+
+has 'name' => (is => 'rw', isa => 'Str');
+has 'index' => (is => 'rw', isa => 'Int');
+has 'number' => (is => 'rw', isa => 'Int');
+has 'type' => (is => 'rw');  # TODO(bradfitz): isa what?
+
+package Protobuf::MessageOptions;
+use Moose;
+
+package Protobuf::Descriptor;
+use Moose;
+
+has 'fields' => (is => 'rw', isa => 'ArrayRef[Protobuf::FieldDescriptor]');
+
+sub fields_by_name {
+    my ($self, $field_name) = @_;
+    for my $field (@{ $self->fields }) {
+        if ($field->name eq $field_name) {
+            return $field;
+        }
+    }
+    return undef;
+}
+
+package Protobuf::FieldDescriptor;
+use Moose;
+
+has 'name' => (is => 'rw', isa => 'Str');
+
 1;
