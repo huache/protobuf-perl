9d261e54ba6d89e9062f1c15e41a98208a1612fd
brad <brad@danga.com>
2008-07-27 18:13:42 +0000

    more

--- perl/lib/Protobuf.pm
+++ perl/lib/Protobuf.pm
@@ -50,6 +50,8 @@ has 'enum_type' => (is => 'rw', isa => 'Maybe[Protobuf::EnumDescriptor]');
 # label can be 1 (optional), 2 (required), 3 (repeated)
 has 'label' => (is => 'rw', isa => 'Int'); # TODO(bradfitz): but only 1, 2, or 3.
 
+has 'type' => (is => 'rw', isa => 'Int'); # TODO(bradfitz): but only [1,18]. see descriptor.proto
+
 sub is_repeated {
     my $self = shift;
     return $self->label == 3;
@@ -65,6 +67,14 @@ sub is_required {
     return $self->label == 2;
 }
 
+sub is_aggregate {
+    my $self = shift;
+    my $type = $self->type;
+    # values from descriptor.proto: (TYPE_GROUP and TYPE_MESSAGE)
+    # http://protobuf.googlecode.com/svn/trunk/src/google/protobuf/descriptor.proto
+    return $type == 10 || $type == 11;
+}
+
 package Protobuf::Message;
 use strict;
 
@@ -74,7 +84,7 @@ sub GenerateClass {
     my %methods;
     $methods{new} = sub {
         my ($class, %param) = @_;
-        $class->meta->new_object(%param);
+        return $class->meta->new_object(%param);
     };
 
     foreach my $field_des (@{ $descriptor->fields }) {
@@ -82,14 +92,31 @@ sub GenerateClass {
         warn "# in $name, field: $name\n";
         if ($field_des->is_repeated) {
             $methods{"add_$name"} = sub {
+                if ($field_des->is_aggregate) {
+                    warn "# adding a repeated aggregate for $name\n";
+                }
+                
                 # TODO(bradfitz): implement. :)
             };
             $methods{"${name}s"} = sub {
                 # TODO(bradfitz): return an arrayref.
+                return [];
             };
+            $methods{"${name}_size"} = sub {
+                my $self = shift;
+                my $method_name = "${name}s";
+                return scalar @{ $self->$method_name };
+            };
+
         } else {
-            # TODO(bradfitz): implement. :)
-            # do normal ->foo and ->set_foo (and ->has_foo) accessors.
+            my $attr = Class::MOP::Attribute->new(
+                ('$' . $name) => (
+                    reader => $name,
+                    writer => "set_$name",
+                    predicate => "has_$name",
+                    # default => TODO(bradfitz): get default from fielddescriptor
+                ));
+            push @attributes, $attr;
         }
     }
 
