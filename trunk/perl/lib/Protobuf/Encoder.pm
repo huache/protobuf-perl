276c64c9f985f6572b695675d6edde2da54f3b86
nothingmuch@woobling.org <nothingmuch@woobling.org@95d14f3d-5152-0410-9fc5-bd637da83747>
2008-07-28 03:17:51 +0000

    group encoding

--- trunk/perl/lib/Protobuf/Encoder.pm
+++ trunk/perl/lib/Protobuf/Encoder.pm
@@ -23,7 +23,7 @@ sub encode_varint {
     return $buf . chr($int);
 }
 
-sub encode_varbuf {
+sub encode_string {
     my ( $self, $buf ) = @_;
 
     die "unexpected utf8 string, should be octets" if utf8::is_utf8($buf); # FIXME what's the correct behavior? at any rate length() needs to be per byte
@@ -31,6 +31,38 @@ sub encode_varbuf {
     return ( $self->encode_varint(length($buf)) . $buf );
 
     # pack("W/a*")
+}
+
+sub encode_4_byte_field {
+    my ( $self, $field, $buf ) = @_;
+    die "value must be a 4 octet string" if utf8::is_utf8($buf) || length($buf) != 4;
+    $self->encode_field_and_wire($field, 5) . $buf;
+}
+
+sub encode_8_byte_field {
+    my ( $self, $field, $buf ) = @_;
+    die "value must be an 8 octet string" if utf8::is_utf8($buf) || length($buf) != 8;
+    $self->encode_field_and_wire($field, 1) . $buf;
+}
+
+sub encode_varint_field {
+    my ( $self, $field, $int ) = @_;
+    $self->encode_field_and_wire($field, 0) . $self->encode_varint($int);
+}
+
+sub encode_string_field {
+    my ( $self, $field, $string ) = @_;
+    $self->encode_field_and_wire($field, 2) . $self->encode_string($string);
+}
+
+sub encode_start_group {
+    my ( $self, $field ) = @_;
+    $self->encode_field_and_wire($field, 3);
+}
+
+sub encode_end_group {
+    my ( $self, $field ) = @_;
+    $self->encode_field_and_wire($field, 4);
 }
 
 __PACKAGE__
