8396e7f9c64c79d29350bdbca2aa51bff0a0f17e
brad <brad@danga.com>
2008-07-28 03:40:40 +0000

    wire format stuff

---
+++ perl/lib/Protobuf/WireFormat.pm
@@ -0,0 +1,13 @@
+package Protobuf::WireFormat;
+use strict;
+use warnings;
+
+sub zigzag_encode {
+    my ($v) = @_;
+    if ($v >= 0) {
+        return $v << 1;
+    }
+    return (($v << 1) ^ (~0)) | 0x1;
+}
+
+1;
