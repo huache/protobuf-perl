bc78e57b7518173766d3830e44db2c0b048c006e
nothingmuch <nothingmuch@woobling.org>
2008-07-28 06:08:40 +0000

    brain fart

--- perl/lib/Protobuf/Attribute/Field/Scalar.pm
+++ perl/lib/Protobuf/Attribute/Field/Scalar.pm
@@ -7,8 +7,8 @@ sub _process_options {
     my ( $class, $name, $options ) = @_;
 
     $options->{reader} = $name;
+    $options->{writer} = "set_$name";
     $options->{predicate} = "has_$name";
-    $options->{predicate} = "set_$name";
 }
 
 sub Moose::Meta::Attribute::Custom::Trait::Protobuf::Field::Scalar::register_implementation { __PACKAGE__ }
